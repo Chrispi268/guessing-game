0bd3785437ba17a4c606dbc0a0d70103bf357a3d
Chrispi <kristinazholud@gmail.com>
2019-10-13 13:20:34 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        minNumeral: 0;
+        maxNumeral: 0;
+            midNumeral: 0;
+    }
 
     setRange(min, max) {
-
+        this.minNumeral = min;
+        this.maxNumeral = max;
     }
 
     guess() {
-
+        this.midNumeral = Math.round((this.minNumeral + this.maxNumeral) / 2);
+        return this.midNumeral;
     }
 
     lower() {
-
+        this.maxNumeral = this.midNumeral;
     }
 
     greater() {
-
+        this.minNumeral = this.midNumeral;
     }
 }
 
